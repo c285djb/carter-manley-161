c3d81c404da8d581126166f67090d02255147a4e
c285djb <manleyc1@gmatc.matc.edu>
2023-09-12 08:28:44 -0500

    Second Commit Act.3

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 import express from 'express';
 import connectDatabase from './config/db';
 
+
 const app = express();
 
+
 connectDatabase();
+
+
+app.use(express.json({ extended: false }));
+
+
 
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpont')
 );
+
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
 
 app.listen(4000, () => console.log ('Express server running on port 4000'));
