d29e4bac80e58cf2aa472f5cba04f408feb1d7c6
c285djb <manleyc1@gmatc.matc.edu>
2023-10-21 16:00:23 -0500

    Activity 7 Complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import './App.css';
 import axios from 'axios';
 import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './App.css';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+
 
 class App extends React.Component {
   state = {
@@ -43,9 +46,8 @@ class App extends React.Component {
           {this.state.data}
         </Route>
         <Switch>
-        <Route path ="/login">
-          Login
-        </Route>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
         </Switch>
       </main>
     </div>
